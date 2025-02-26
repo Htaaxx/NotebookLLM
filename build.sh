91bdf9c2c1d12d9233faa561fbba30fd0de2dc66
Nghia Vo <voasd00@gmail.com>
2025-02-26 11:09:39 +0700

    Refactor vercel.json and build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-
-# Build the project
-echo "Building the project..."
-python3.12 -m pip install -r requirements.txt
+python3.12 -m pip install --disable-pip-version-check --target . --upgrade -r requirements.txt
