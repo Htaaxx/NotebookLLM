6bb358f259cdcfc9be043153ed5016dcc785b718
Nghia Vo <voasd00@gmail.com>
2025-02-26 12:13:45 +0700

    refactor main.py

--- api/main.py
+++ api/main.py
@@ -29,5 +29,5 @@ async def read_root():
 # async def extract_text_api(file: UploadFile = File(...)):
 #     return extract_text(file)
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
