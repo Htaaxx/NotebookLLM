f94b143c08b97f35b0b6e37d372c0707470d0b2f
Nghia Vo <voasd00@gmail.com>
2025-04-02 22:55:21 +0700

    add root direct in server.js

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -22,7 +22,12 @@ mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB Atlas"))
   .catch(err => console.error("MongoDB Connection Error:", err));
 
+
 // Routes
+// root route
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 app.use("/api", webRoutes);
 app.use("/user", uploadRoutes); // Cloudinary
 
