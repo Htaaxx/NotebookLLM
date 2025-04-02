497e095c5669b444cbb4f49b9d1194f1bf3b85aa
Nghia Vo <voasd00@gmail.com>
2025-04-03 00:30:53 +0700

    Refactor server.js so it could listen to external access

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -32,4 +32,4 @@ app.use("/api", webRoutes);
 app.use("/user", uploadRoutes); // Cloudinary
 
 // Start server
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () => console.log(`Server running on port ${PORT}`));
