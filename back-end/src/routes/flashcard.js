5d7d67ec6ae4bd7c26bfc6e91353cff4befb3a6e
WanWin04 <123612410+WanWin04@users.noreply.github.com>
2025-04-09 18:16:29 +0700

    --reload

--- back-end/src/routes/flashcard.js
+++ back-end/src/routes/flashcard.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const { generateFlashcards } = require("../controllers/flashcardController");
+// const { saveFlashcards } = require("../controllers/authController");
 
 const router = express.Router();
 
 router.post("/flashcards", generateFlashcards);
+// router.post("/save-flashcards", saveFlashcards);
 
 module.exports = router;
