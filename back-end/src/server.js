55652ad3fa9c4f1ea13d6b96345a41ad23741b76
Nghia Vo <voasd00@gmail.com>
2025-04-14 01:07:36 +0700

    update dockerfile in back-end and change PORT to PORT_BACKEND

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -9,7 +9,7 @@ const flashcardRoutes = require("./routes/flashcard");
 const reviewRoutes = require("./routes/review");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT_BACKEND || 5000;
 // Change the URI to your own database.
 const MONGO_URI = process.env.MONGO_URI ;
 
